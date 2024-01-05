ac8354610f0a58b2045b2d0d95089b8eb8fb0c6d
siddankthep <quan.nguyen.210257@student.fulbright.edu.vn>
2024-01-05 16:28:57 +0700

    add subwallet docs

--- sidebars.js
+++ sidebars.js
@@ -28,10 +28,9 @@ module.exports = {
       {
         type: "category",
         label: "Wallets",
-        items: [
-          "create_account",
-          "howto_wallet_parity_signer"
-        ]
+        items: ["create_account", 
+        "howto_wallet_subwallet", 
+        "howto_wallet_parity_signer"],
       },
       {
         type: "category",
@@ -39,8 +38,8 @@ module.exports = {
         items: [
           "tip_request",
           "participate_in_referenda",
-          "participate_in_council_elections"
-        ]
+          "participate_in_council_elections",
+        ],
       },
       "claim",
       "identity",
@@ -48,9 +47,9 @@ module.exports = {
         type: "category",
         label: "Connect to a Node",
         items: ["polkadotjs_apps_local", "polkadotjs_apps_public"],
-      }
+      },
     ],
-    "Learn": [
+    Learn: [
       "learn_amm",
       {
         type: "category",
@@ -59,27 +58,18 @@ module.exports = {
           "democracy_intro",
           "democracy_referenda",
           "democracy_council",
-          "democracy_technical_committee"
-        ]
+          "democracy_technical_committee",
+        ],
       },
       {
         type: "category",
         label: "Omnipool",
-        items: [
-          "omnipool_design"
-        ]
-      }
+        items: ["omnipool_design"],
+      },
     ],
-    "Maintain": [
-      "collator_setup",
-      "performance_benchmark",
-      "node_monitoring"
-    ],
-    Build: [
-      "dev_intro",
-      "build_dev_chain",
-    ],
+    Maintain: ["collator_setup", "performance_benchmark", "node_monitoring"],
+    Build: ["dev_intro", "build_dev_chain"],
     Archive: ["archive_hydradx_crowdloan"],
     Contribute: ["contributing"],
-  }
+  },
 };
